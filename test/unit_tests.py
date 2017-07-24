587d9ca839402882d19c7bed627d91cf189b2a9b
rob <robert.osterburg@gmail.com>
2017-07-23 19:05:06 -0600

    Changes resulting from function name refactoring

--- test/unit_tests.py
+++ test/unit_tests.py
@@ -6,7 +6,7 @@ def test_create_url():
     dt = datetime(year=2017, month=6, day=1, hour=12, tzinfo=timezone.utc)
     maptype = 'usfntsfc'
     expected = 'http://www.wpc.ncep.noaa.gov/archives/web_pages/sfc/sfc_archive_maps.php?arcdate=06/01/2017&selmap=2017060112&maptype=usfntsfc'
-    assert (wpc_map_scraper.build_page_url(dt, maptype)) == expected
+    assert (wpc_map_scraper._build_page_url(dt, maptype)) == expected
 
 
 def test_make_iso_date():
@@ -14,7 +14,7 @@ def test_make_iso_date():
     date_str = f'{y:04d}-{m:02d}-{d:02d}'
     time_str = f'{h:02d}'
     expected = datetime(year=y, month=m, day=d, hour=h, tzinfo=timezone.utc)
-    assert(wpc_map_scraper.make_iso_date(date_str, time_str)) == expected
+    assert (wpc_map_scraper._make_iso_date(date_str, time_str)) == expected
 
 
 def test_build_no_time_series():
@@ -22,23 +22,23 @@ def test_build_no_time_series():
     start = '2017-01-01'
     stop = '2016-12-31'
     times = []
-    assert(wpc_map_scraper.make_time_series(start, stop, times)) == []
+    assert (wpc_map_scraper._make_time_series(start, stop, times)) == []
 
 
 def test_build_time_series():
     start = '2016-12-31'
     stop = '2017-01-01'
     times = [0, 6, 18]
-    assert(len(wpc_map_scraper.make_time_series(start, stop, times))) == 6
+    assert (len(wpc_map_scraper._make_time_series(start, stop, times))) == 6
 
 
 def test_short_build_time_series():
     start = '2017-01-01'
     stop = '2017-01-01'
     times = ['06', '18']
-    actual = wpc_map_scraper.make_time_series(start, stop, times)
-    start_dt = wpc_map_scraper.make_iso_date(start, times[0])
-    stop_dt = wpc_map_scraper.make_iso_date(stop, times[1])
+    actual = wpc_map_scraper._make_time_series(start, stop, times)
+    start_dt = wpc_map_scraper._make_iso_date(start, times[0])
+    stop_dt = wpc_map_scraper._make_iso_date(stop, times[1])
     assert(len(actual)) == 2
     assert(start_dt in actual)
     assert(stop_dt in actual)
@@ -48,20 +48,20 @@ def test_page_url():
     expected = 'http://www.wpc.ncep.noaa.gov/archives/web_pages/sfc/sfc_archive_maps.php?arcdate=06/15/2017&selmap=2017061521&maptype=usfntsfc'
     date_str = '2017-06-15'
     time_str = '21'
-    date_time = wpc_map_scraper.make_iso_date(date_str, time_str=time_str)
-    assert (wpc_map_scraper.build_page_url(date_time, 'usfntsfc')) == expected
+    date_time = wpc_map_scraper._make_iso_date(date_str, time_str=time_str)
+    assert (wpc_map_scraper._build_page_url(date_time, 'usfntsfc')) == expected
 
 
 def test_map_path():
     expected = '/Users/rob/Downloads/wx_maps/20170601_12z_namussfc.gif'
     dt = datetime(year=2017, month=6, day=1, hour=12, tzinfo=timezone.utc)
     maptype = 'namussfc'
-    assert(wpc_map_scraper.get_map_path('~/Downloads/wx_maps', dt, maptype)) == expected
+    assert (wpc_map_scraper._get_map_path('~/Downloads/wx_maps', dt, maptype)) == expected
 
 
 def test_build_image_url():
     expected = 'http://www.wpc.ncep.noaa.gov/archives/sfc/2017/namussfc2017060112.gif'
     dt = datetime(year=2017, month=6, day=1, hour=12, tzinfo=timezone.utc)
     maptype = 'namussfc'
-    page_url = wpc_map_scraper.build_page_url(dt, maptype)
-    assert(wpc_map_scraper.build_image_url(page_url)) == expected
+    page_url = wpc_map_scraper._build_page_url(dt, maptype)
+    assert (wpc_map_scraper._build_image_url(page_url)) == expected
