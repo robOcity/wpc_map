90385b860217f5a14fc42f3e740ae5d481a0ed61
rob <robert.osterburg@gmail.com>
2017-07-26 20:43:53 -0600

    Moved tests functions to top and made utility functions private

--- test/functional_tests.py
+++ test/functional_tests.py
@@ -2,35 +2,25 @@ import wpc_map_scraper as wpc
 import os
 
 
-def test_scrape_map_sequence(tmpdir):
-    # create a sequence of events with two occurrences on two days
-    expected, paths = scrape_maps(tmpdir, '2017-06-30', '2017-07-01', ['00', '12'], ['usfntsfc', 'ussatsfc'])
-
-    # perform tests
-    check_numbers(expected, paths, tmpdir)
-    check_files_exist(paths)
-
-
 def test_scrape_map(tmpdir):
     # create a sequence of events with two occurrences on two days
-    expected, paths = scrape_maps(tmpdir, '2017-07-04', '2017-07-04', ['18'], ['namussfc'])
+    expected, paths = _scrape_maps('2017-07-04', '2017-07-04', '24', ['namussfc'], tmpdir)
 
     # perform tests
-    check_numbers(expected, paths, tmpdir)
-    check_files_exist(paths)
+    _check_numbers(expected, paths, tmpdir)
+    _check_files_exist(paths)
 
 
-def check_files_exist(paths):
-    for path in paths:
-        assert (os.path.exists(path))
+def test_scrape_map_sequence(tmpdir):
+    # create a sequence of events with two occurrences on two days
+    expected, paths = _scrape_maps('2017-06-30', '2017-07-01', '12', ['usfntsfc', 'ussatsfc'], tmpdir)
+
+    # perform tests
+    _check_numbers(expected, paths, tmpdir)
+    _check_files_exist(paths)
 
 
-def check_numbers(expected, paths, tmpdir):
-    assert (len(paths)) == expected
-    assert (len(tmpdir.listdir())) == expected
-
-
-def scrape_maps(tmpdir, start, stop, times, maps):
+def _scrape_maps(tmpdir, start, stop, times, maps):
     # create a sequence of events with two occurrences on two days
     dates = [date for date in wpc._make_time_series(start, stop, times)]
 
@@ -41,5 +31,15 @@ def scrape_maps(tmpdir, start, stop, times, maps):
     wpc.scrape_map(start, stop, times, maps, tmpdir)
     expected_num = len(dates) * len(maps)
     return expected_num, paths
+
+
+def _check_files_exist(paths):
+    for path in paths:
+        assert (os.path.exists(path))
+
+
+def _check_numbers(expected, paths, tmpdir):
+    assert (len(paths)) == expected
+    assert (len(tmpdir.listdir())) == expected
 
 
