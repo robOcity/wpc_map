cdee9bf9bbf2dd30dfad5c448625e03ed488361d
rob <robert.osterburg@gmail.com>
2017-07-28 06:58:04 -0600

    Functional tests updated and running successfully

--- test/functional_tests.py
+++ test/functional_tests.py
@@ -1,38 +1,43 @@
-import wx_map as wpc
+import wx_map as wx
 import os
+from click.testing import CliRunner
 
 
 def test_scrape_map(tmpdir):
     # create a sequence of events with two occurrences on two days
-    expected, paths = _scrape_maps('2017-07-04', '2017-07-04', '24', ['namussfc'], tmpdir)
+    start, end, period, maps, map_dir = '2017-07-04', '2017-07-04', '24', ['namussfc'], tmpdir
+    dates = [date for date in wx._make_time_series(start, end, period)]
+
+    # create the unique file names for each map and time
+    paths = [wx._get_map_path(tmpdir, date, _map)
+             for date in dates
+             for _map in maps]
+
+    # get the maps
+    runner = CliRunner()
+    result = runner.invoke(wx.cli, ['-s', start, '-e', end, '-p', period, '-m', 'namussfc', '-d', os.path.abspath(tmpdir)])
 
     # perform tests
-    _check_numbers(expected, paths, tmpdir)
+    _check_numbers(paths, tmpdir)
     _check_files_exist(paths)
 
 
 def test_scrape_map_sequence(tmpdir):
     # create a sequence of events with two occurrences on two days
-    expected, paths = _scrape_maps('2017-06-30', '2017-07-01', '12', ['usfntsfc', 'ussatsfc'], tmpdir)
-
-    # perform tests
-    _check_numbers(expected, paths, tmpdir)
-    _check_files_exist(paths)
-
-
-def _scrape_maps(start, stop, period, maps, tmpdir):
-    # create a sequence of events with two occurrences on two days
-    dates = [date for date in wpc._make_time_series(start, stop, period)]
+    start, end, period, maps, map_dir = '2017-06-30', '2017-07-01', '12', ['usfntsfc', 'ussatsfc'], tmpdir
+    dates = [date for date in wx._make_time_series(start, end, period)]
 
     # create the unique file names for each map and time
-    paths = [wpc._get_map_path(tmpdir, date, map)
+    paths = [wx._get_map_path(tmpdir, date, _map)
              for date in dates
-             for map in maps]
+             for _map in maps]
 
-    # get the maps
-    wpc.cli(start, stop, period, maps, tmpdir)
-    expected_num = len(dates) * len(maps)
-    return expected_num, paths
+    runner = CliRunner()
+    result = runner.invoke(wx.cli, ['-s', start, '-e', end, '-p', period, '-m', maps[0], '-m', maps[1], '-d', os.path.abspath(tmpdir)])
+
+    # perform tests
+    _check_numbers(paths, tmpdir)
+    _check_files_exist(paths)
 
 
 def _check_files_exist(paths):
@@ -40,8 +45,6 @@ def _check_files_exist(paths):
         assert (os.path.exists(path))
 
 
-def _check_numbers(expected, paths, tmpdir):
-    assert (len(paths)) == expected
-    assert (len(tmpdir.listdir())) == expected
-
-
+def _check_numbers(expected, tmpdir):
+    actual_maps = tmpdir.listdir()
+    assert (len(actual_maps)) == len(expected)
