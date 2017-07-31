443927eab18ca4c37eeff70395fbaba9668db682
rob <robert.osterburg@gmail.com>
2017-07-31 15:54:25 -0600

    adding initial version

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='wx_map',
+    version='1.0.0',
+    packages=['wx_map'],
+    url='https://github.com/robOcity/wx_map',
+    license='MIT',
+    author='Robert Osterburg',
+    author_email='robert.osterburg@gmail.com',
+    description='Downloads archived weather maps from the Weather Prediction Center (WPC).'
+)
