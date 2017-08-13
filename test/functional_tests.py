421d527a75dbb99166ff80711c9d1a873f74984f
Rob Osterburg <rober.osterburg@gmail.com>
2017-08-12 20:48:07 -0600

    updated for new package naming

--- test/functional_tests.py
+++ test/functional_tests.py
@@ -2,22 +2,22 @@ import os
 
 from click.testing import CliRunner
 
-import wx_map as wx
+from wx_map import cli
 
 
 def test_scrape_map(tmpdir):
     # create a sequence of events with two occurrences on two days
     start, end, period, maps, map_dir = '2017-07-04', '2017-07-04', '24', ['namussfc'], tmpdir
-    dates = [date for date in wx._make_time_series(start, end, period)]
+    dates = [date for date in cli._make_time_series(start, end, period)]
 
     # create the unique file names for each map and time
-    paths = [wx._get_map_path(tmpdir, date, _map)
+    paths = [cli._get_map_path(tmpdir, date, _map)
              for date in dates
              for _map in maps]
 
     # get the maps
     runner = CliRunner()
-    result = runner.invoke(wx.cli, ['-s', start, '-e', end, '-p', period, '-m', 'namussfc', '-d', os.path.abspath(tmpdir)])
+    result = runner.invoke(cli.get, ['-s', start, '-e', end, '-p', period, '-m', 'namussfc', '-d', os.path.abspath(tmpdir)])
 
     # perform tests
     _check_numbers(paths, tmpdir)
@@ -27,15 +27,15 @@ def test_scrape_map(tmpdir):
 def test_scrape_map_sequence(tmpdir):
     # create a sequence of events with two occurrences on two days
     start, end, period, maps, map_dir = '2017-06-30', '2017-07-01', '12', ['usfntsfc', 'ussatsfc'], tmpdir
-    dates = [date for date in wx._make_time_series(start, end, period)]
+    dates = [date for date in cli._make_time_series(start, end, period)]
 
     # create the unique file names for each map and time
-    paths = [wx._get_map_path(tmpdir, date, _map)
+    paths = [cli._get_map_path(tmpdir, date, _map)
              for date in dates
              for _map in maps]
 
     runner = CliRunner()
-    result = runner.invoke(wx.cli, ['-s', start, '-e', end, '-p', period, '-m', maps[0], '-m', maps[1], '-d', os.path.abspath(tmpdir)])
+    result = runner.invoke(cli.get, ['-s', start, '-e', end, '-p', period, '-m', maps[0], '-m', maps[1], '-d', os.path.abspath(tmpdir)])
 
     # perform tests
     _check_numbers(paths, tmpdir)
