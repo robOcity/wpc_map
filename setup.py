1799c0836f87c77492ded115c45947f69aba1aee
Rob Osterburg <rober.osterburg@gmail.com>
2017-08-12 19:15:45 -0600

    Adding __init__.py files

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     license='MIT',
     author='Robert Osterburg',
     author_email='robert.osterburg@gmail.com',
-    description=('Download a sequence of archived weather maps',
-                 'from the Weather Prediction Center (WPC).'),
+    description=('Download a sequence of archived weather maps \
+    from the Weather Prediction Center (WPC).'),
     keywords=['weather', 'map', 'surface'],
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
@@ -24,10 +24,12 @@ setup(
     long_description="""\
 Download a sequence of archived weather maps from the Weather Prediction Center (WPC).
 
-Surface weather maps for North America and the Continental United States are
-available from May 1, 2005 onward.  Eight different kinds of maps are available at eight different times each day.
-This program will allow you to download one type of map for a range of dates, and store the maps in a folder. 
-"""
+Surface weather maps for North America and the Continental United States 
+are available from May 1, 2005 onward for most maps.  Eight different 
+kinds of maps are available at eight different times each day.  This 
+program will allow you to download one type of map for a range of dates, 
+and store the maps in a folder. 
+""",
 )
 
 
